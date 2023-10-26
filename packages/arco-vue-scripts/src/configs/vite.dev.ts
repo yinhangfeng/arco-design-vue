c1a5ac7d51f13caa71b510cb39aa9556bbf91d2c
yinhangfeng <yinhangfeng@gmail.com>
2023-10-26 16:30:27 +0800

    feat: arco-design-ns

--- packages/arco-vue-scripts/src/configs/vite.dev.ts
+++ packages/arco-vue-scripts/src/configs/vite.dev.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     target: 'modules',
     outDir: 'es',
-    emptyOutDir: true,
+    emptyOutDir: false,
     minify: false,
     brotliSize: false,
     rollupOptions: {
