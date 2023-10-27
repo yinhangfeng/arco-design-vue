d9d5a0ae89c7770c25e071997324088bbbf6a72f
yinhangfeng <yinhangfeng@gmail.com>
2023-10-27 11:49:06 +0800

    feat: export configProviderInjectionKey

--- packages/web-vue/components/config-provider/index.ts
+++ packages/web-vue/components/config-provider/index.ts
@@ -1,9 +1,11 @@
 import type { App } from 'vue';
 import type { ArcoOptions } from '../_utils/types';
 import { setGlobalConfig, getComponentPrefix } from '../_utils/global-config';
+import { configProviderInjectionKey } from './context';
 import _ConfigProvider from './config-provider.vue';
 
 const ConfigProvider = Object.assign(_ConfigProvider, {
+  configProviderInjectionKey,
   install: (app: App, options?: ArcoOptions) => {
     setGlobalConfig(app, options);
     const componentPrefix = getComponentPrefix(options);
